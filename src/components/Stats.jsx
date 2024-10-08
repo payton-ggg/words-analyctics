42aa64d1a56b26d99d0c219eb2b683cb0a72cb96
Platon <plato@hisoka.com>
2024-10-08 13:49:31 +0300

    asdfsa

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -14,7 +14,11 @@ const Stats = ({ numberOfCharacters, words }) => {
 function Stat({ number, label }) {
   return (
     <section className="stat">
-      <span className="stat__number">{number}</span>
+      <span
+        className={`stat__number ${number < 0 ? "stat__number--limit" : ""}`}
+      >
+        {number}
+      </span>
       <h2 className="second-heading">{label}</h2>
     </section>
   );
